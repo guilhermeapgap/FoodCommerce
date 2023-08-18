2f12fdcb205708a22008302b9a8eb823d0c093d8
Guilherme <guilhermeapgap@hotmail.com>
2023-08-17 21:37:08 -0300

    nav

--- food-commerce/src/components/Sidebar/index.tsx
+++ food-commerce/src/components/Sidebar/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
+
 import { Container } from "./styles"
 
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 import menuImg from '../../assets/menu.svg'
 
 export function Sidebar(){
-    return <Container>
-        <button type="button">
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleToggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+    return <Container isMenuOpen={menuOpen}>
+        <button type="button" onClick={handleToggleMenu}>
             <img src={menuImg} alt="abrir e fechar menu"/>
         </button>
         <nav>
@@ -15,6 +27,24 @@ export function Sidebar(){
                     <a href="#" className="active">
                         <BurgerIcon/>
                         <span>Hambúrgueres</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <PizzaIcon/>
+                        <span>Pizzas</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <SodaPopIcon/>
+                        <span>Bebidas</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <IceCreamIcon/>
+                        <span>Sorvetes</span>
                     </a>
                 </li>
             </ul>
