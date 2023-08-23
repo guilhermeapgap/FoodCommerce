2fccc9541f1a23a089d14005a8fcbdb732d6e127
Guilherme <guilhermeapgap@hotmail.com>
2023-08-23 20:27:00 -0300

    array de burgues

--- food-commerce/src/pages/Main/Burgers/index.tsx
+++ food-commerce/src/pages/Main/Burgers/index.tsx
@@ -2,6 +2,26 @@ import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
 
 export default function Burgers() {
+
+  const data = [
+    {
+      id: 1,
+      snack: 'burguer',
+      name: 'Mega',
+      description: 'Artesanal grande',
+      price: 25.5,
+      Image:'https://i.imgur.com/upjIUnG.jpg'
+    },
+    {
+      id: 2,
+      snack: 'burguer',
+      name: 'Extra bacon',
+      description: 'Artesanal pequeno',
+      price: 15.5,
+      Image:'https://i.imgur.com/B4J04AJ.jpg'
+    },
+  ]
+
   return (
     <>
       <Head title='Hambúrgueres'/>
