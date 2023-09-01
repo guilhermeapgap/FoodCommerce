862369cd453e7d7e6e7ea2f2e7cb55441b1155ab
Guilherme <Guilhermeapgap@hotmail.com>
2023-09-01 20:22:08 -0300

    add loading

--- food-commerce/src/services/api.ts
+++ food-commerce/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  //baseURL: 'http://localhost:3000/'
+  //baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'http://localhost:5000/'
 })
 
 
