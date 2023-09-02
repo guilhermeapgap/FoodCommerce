c0e3386da487b885659ec6f9927675fefa7ebfe5
Guilherme <Guilhermeapgap@hotmail.com>
2023-09-02 20:20:19 -0300

    finalizando loading

--- food-commerce/src/components/Snacks/index.tsx
+++ food-commerce/src/components/Snacks/index.tsx
@@ -1,5 +1,6 @@
 import { FiPlus } from 'react-icons/fi'
 
+import { SkeletonSnack } from './SkeletonSnack'
 import { Container } from "./styles"
 import { currencyFormat } from '../../helpers/currencyFormat'
 
@@ -12,23 +13,21 @@ interface SnacksProps {
 export function Snacks({ snacks }: SnacksProps) {
     return (
         <Container>
-            {!snacks.length ? (
-                <p>Loading...</p>
-            ) : (
-                snacks.map((snack) => (
-                    <div key={snack.id} className="snack">
-                        <h2>{snack.name}</h2>
-                        <img src={snack.image} alt={snack.name} />
-                        <p>{snack.description}</p>
-                        <div>
-                            <strong>{currencyFormat(snack.price)}</strong>
-                            <button type="button">
-                                <FiPlus />
-                            </button>
-                        </div>
-                    </div>
-                ))
-            )}
-        </Container>
+        {!snacks.length
+          ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+          : snacks.map((snack) => (
+              <div key={snack.id} className='snack'>
+                <h2>{snack.name}</h2>
+                <img src={snack.image} alt={snack.name} />
+                <p>{snack.description}</p>
+                <div>
+                  <strong>{currencyFormat(snack.price)}</strong>
+                  <button type='button'>
+                    <FiPlus />
+                  </button>
+                </div>
+              </div>
+            ))}
+      </Container>
     )
 }
