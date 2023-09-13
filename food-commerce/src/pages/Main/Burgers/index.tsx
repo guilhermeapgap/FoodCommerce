db154f1eb64cae78873589e31b4660657719915e
Guilherme <guilhermeapgap@hotmail.com>
2023-09-13 11:07:52 -0300

    finalizando o context

--- food-commerce/src/pages/Main/Burgers/index.tsx
+++ food-commerce/src/pages/Main/Burgers/index.tsx
@@ -1,16 +1,12 @@
-import { useState, useEffect, useContext } from 'react'
+import { useSnack } from '../../../hooks/useSnack'
 
 import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
 import { Snacks } from '../../../components/Snacks'
 
-import { getBurgers } from '../../../services/api'
-import { SnackData } from '../../../interfaces/SnackData'
-import { SnackContext } from '../../../App'
-
 export default function Burgers() {
 
-  const {burgers} = useContext(SnackContext)
+  const {burgers} = useSnack()
 
   return (
     <>
--- food-commerce/src/pages/Main/Drinks/index.tsx
+++ food-commerce/src/pages/Main/Drinks/index.tsx
@@ -1,11 +1,12 @@
-import { useContext } from 'react'
-import { SnackContext } from '../../../App'
+import { useSnack } from '../../../hooks/useSnack'
+
 import { Head } from '../../../components/Head'
 import { Snacks } from '../../../components/Snacks'
 import { SnackTitle } from '../../../components/SnackTitle'
 
 export default function Drinks() {
-  const { drinks } = useContext(SnackContext)
+  const { drinks } = useSnack()
+
 
   return (
     <>
