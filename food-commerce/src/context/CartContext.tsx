2ba1779e93134bc1c15412597228f9591cbcb004
Guilherme <guilhermeapgap@hotmail.com>
2023-09-14 23:05:49 -0300

    carrinhos

--- food-commerce/src/context/CartContext.tsx
+++ food-commerce/src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 import { SnackData } from '../interfaces/SnackData'
 
@@ -21,8 +21,8 @@ interface UpdateCartProps {
 interface CartContextProps {
   cart: Snack[]
   addSnackIntoCart: (snack: SnackData) => void
-  removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void
-  updateCart: ({ id, snack, newQuantity }: UpdateCartProps) => void
+  // removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void
+  // updateCart: ({ id, snack, newQuantity }: UpdateCartProps) => void
 }
 
 interface CartProviderProps {
@@ -32,5 +32,14 @@ interface CartProviderProps {
 export const CartContext = createContext({} as CartContextProps)
 
 export function CartProvider({ children }: CartProviderProps) {
-  //
+  const [cart, setCart] = useState<Snack[]>([])
+
+  function addSnackIntoCart(snack: SnackData): void {
+    const newSnack = { ...snack, quantity: 1, subtotal: snack.price }
+    const newCart = [...cart, newSnack] // push de um array
+
+    setCart(newCart)
+  }
+
+  return <CartContext.Provider value={{ cart, addSnackIntoCart }}>{children}</CartContext.Provider>
 }
