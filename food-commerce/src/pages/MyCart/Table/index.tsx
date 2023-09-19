074bf3b333ce0cf21b3b211c264f9d64388f997c
Guilherme <guilhermeapgap@hotmail.com>
2023-09-18 23:36:14 -0300

    cartr

--- food-commerce/src/pages/MyCart/Table/index.tsx
+++ food-commerce/src/pages/MyCart/Table/index.tsx
@@ -5,7 +5,7 @@ import { TableDesktop } from './TableDesktop'
 export function Table() {
   const { cart } = useCart()
 
-  if (cart.length === 0) return <h1>Ops! Parece que você não tem pedidos, peça já!</h1>
+  if (cart.length === 0) return <h1>Ops! Parece que você não tem pedidos, peça já!!</h1>
 
   return <TableDesktop />
 }
