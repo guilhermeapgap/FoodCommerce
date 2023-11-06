b437261f2dc023b3d0885249598885e647ee574d
Guilherme <guilhermeapgap@hotmail.com>
2023-11-06 20:08:27 -0300

    validação

--- food-commerce/src/pages/Payment/index.tsx
+++ food-commerce/src/pages/Payment/index.tsx
@@ -1,4 +1,7 @@
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import { IMaskInput } from 'react-imask'
+import * as yup from 'yup'
 
 import { Head } from '../../components/Head'
 import { PayOrder } from '../../components/OrderCloseAction/PayOrder'
@@ -6,18 +9,28 @@ import { OrderHeader } from '../../components/OrderHeader'
 
 import { Container, Form, Inner } from './styles'
 
-type FieldValues = {
-  fullName: string
-  email: string
-  mobile: string
-}
+const schema = yup
+  .object({
+    fullName: yup
+      .string()
+      .required('Nome e sobrenome são obrigatórios.')
+      .min(3, 'Nome e sobrenome muito curto.'),
+    email: yup.string().email().required(),
+    mobile: yup.string().required(),
+  })
+  .required()
+
+type FieldValues = yup.InferType<typeof schema>
 
 export default function Payment() {
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FieldValues>()
+  } = useForm<FieldValues>({
+    resolver: yupResolver(schema),
+  })
   const onSubmit: SubmitHandler<FieldValues> = (data) => console.log('data', data)
 
   return (
@@ -30,24 +43,49 @@ export default function Payment() {
 
           <div className='field'>
             <label htmlFor='fullName'>Nome e sobrenome</label>
-            <input
-              type='text'
-              id='fullName'
-              autoComplete='name'
-              {...register('fullName', { required: true })}
+
+            <Controller
+              name='fullName'
+              control={control}
+              render={({ field }) => (
+                <input type='text' id='fullName' autoComplete='name' {...field} />
+              )}
             />
-            {errors.fullName && <p className='error'>O nome e sobrenome é um campo obrigatório.</p>}
+
+            {errors.fullName && <p className='error'>{errors.fullName.message}</p>}
           </div>
 
           <div className='grouped'>
             <div className='field'>
               <label htmlFor='email'>E-mail</label>
-              <input type='email' name='email' id='email' autoComplete='email' />
+
+              <Controller
+                name='email'
+                control={control}
+                render={({ field }) => (
+                  <input type='text' id='email' autoComplete='email' {...field} />
+                )}
+              />
+
+              {errors.email && <p className='error'>{errors.email.message}</p>}
             </div>
 
             <div className='field'>
               <label htmlFor='mobile'>Celular</label>
-              <input type='tel' id='mobile' name='mobile' autoComplete='phone' />
+              <Controller
+                name='mobile'
+                control={control}
+                render={({ field }) => (
+                  <IMaskInput
+                    type='tel'
+                    id='mobile'
+                    autoComplete='phone'
+                    mask={'(00) 90000-0000'}
+                    {...field}
+                  />
+                )}
+              />
+              {errors.mobile && <p className='error'>{errors.mobile.message}</p>}
             </div>
 
             <div className='field'>
