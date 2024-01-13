59b621491695cb0dcc236f90079764f1a5a95160
Guilherme <guilhermeapgap@hotmail.com>
2024-01-13 14:36:18 -0300

    start backend

---
+++ BackEnd/src/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import express, {Express,Request, Response} from 'express'
+
+dotenv.config()
+
+const app: Express  = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) =>{
+    const {message} = req.body
+
+    if(!message) return res.status(400).send({error: "Message is required"})
+
+    res.send({ message })
+});
+
+app.listen(port, () =>{
+    console.log(`Server runnig on port ${port}`)
+})
